9f74b3cf92fb9b71f7df6b0cf4517ce5b4bd4c15
Lucas Popp <lucaspopp0@gmail.com>
2024-05-31 14:04:43 -0700

    delete dumb stuff

--- downloader/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-IMAGE_NAME=downloader
-
-docker build . -t "${IMAGE_NAME}" || exit 1
-docker run \
-    -v $(pwd):/root \
-    "${IMAGE_NAME}" \
-    spotdl --ffmpeg /root/.spotdl/ffmpeg $@
